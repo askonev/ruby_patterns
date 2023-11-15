2a660976d2ffe3243f012320d54506dbd95344a1
MaxSorokin <askonev007@gmail.com>
2023-11-15 19:38:42 +0300

    feature: push block if

--- githooks.sh
+++ githooks.sh
@@ -5,8 +5,17 @@ chmod +x .git/hooks/pre-push
 
 cat > .git/hooks/pre-push << "EOF"
 #!/bin/bash
-# Run markdownlint before pushing to origin
-markdownlint '**/*.md' --ignore node_modules
-# Exit with the exit status of RuboCop
-exit $?
+if ! git diff --exit-code >> /dev/null; then
+    echo 'All changes must be added to the commit'
+    git status
+    exit 1
+elif ! git diff --staged --exit-code >> /dev/null; then
+    echo 'All changes must be added to the stage'
+    git status
+    exit 1
+else
+    # Run markdownlint before pushing to origin
+    markdownlint '**/*.md' --ignore node_modules
+    exit $?
+fi
 EOF
